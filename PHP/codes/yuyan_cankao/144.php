ec56826cfb864fd362e59a5cafab0b2090efee38
DaiYan <DaiYan@heng.zhun.com.cn>
2017-01-12 19:13:07 +0800

    add files 20170112

---
+++ PHP/codes/yuyan_cankao/144.php
@@ -0,0 +1,7 @@
+<?php
+class a{
+    const A=12;
+}
+
+$clasName="a";
+var_dump($clasName::A);    //变量引用类来访问常量
---
+++ PHP/codes/yuyan_cankao/145.php
@@ -0,0 +1,8 @@
+<?php
+class a{
+    public static $a;
+    const c=122;
+    public static function cc{
+        echo __CLASS__ . ':' . __FUNCTION__;
+    }
+}
