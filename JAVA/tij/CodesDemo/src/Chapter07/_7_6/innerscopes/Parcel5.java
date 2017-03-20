ab8888d7728610173b9b348607ca7a86cf0cff38
CodingWorker <dy10010@sina.cn>
2017-03-20 09:39:35 +0800

    java

---
+++ JAVA/tij/CodesDemo/src/Chapter07/_7_6/innerscopes/Parcel5.java
@@ -0,0 +1,33 @@
+package Chapter07._7_6.innerscopes;
+
+/**
+ * Created by DaiYan on 2017/3/20.
+ */
+public class Parcel5 {
+    public void main(){
+        Parcel5 p=new Parcel5();
+        p.track();
+    }
+
+    private void track(){
+        this.internalTracking(true);
+    }
+
+    private void internalTracking(boolean b){
+        if(b){
+            class TrackingSlip{  //除此if分支块外不能访问此类
+                private String id;
+                TrackingSlip(String s){
+                    id=s;
+                }
+
+                String getSlip(){
+                    return id;
+                }
+            }
+            TrackingSlip ts=new TrackingSlip("slip");
+            String s=ts.getSlip();
+        }
+    }
+
+}
