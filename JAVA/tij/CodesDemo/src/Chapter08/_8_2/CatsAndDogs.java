a453e299ce213d2b23409e40dd036c30e431d9a2
CodingWorker <dy10010@sina.cn>
2017-03-22 09:34:18 +0800

    java

--- JAVA/tij/CodesDemo/src/Chapter08/_8_2/CatsAndDogs.java
+++ JAVA/tij/CodesDemo/src/Chapter08/_8_2/CatsAndDogs.java
@@ -1,5 +1,7 @@
 package Chapter08._8_2;
 
+import com.sun.javafx.geom.transform.CanTransformVec3d;
+
 import java.util.Vector;
 
 /**
@@ -29,7 +31,7 @@ class Dog{
 
 public class CatsAndDogs {
     public static void main(String[] args){
-        Vector cats=new Vector();
+        Vector cats=new Vector();    //不指定元素类型时默认为object，调用cat的方法时需要溯型
         for(int i=0;i<7;i++){
             cats.addElement(new Cat(i));
         }
@@ -39,5 +41,11 @@ public class CatsAndDogs {
         for(int i=0;i<cats.size();i++){
             ((Cat)cats.get(i)).print();
         }
+
+        Vector<Cat> catVector=new Vector<>();
+        catVector.addElement(new Cat(1));
+        //catVector.addElement(Dog(2));  //类型不匹配
+        catVector.elementAt(1).print();   //因为catVector限制了元素只能是cat类型，所以访问元素
+        // 的时候就是cat不用再次的溯型
     }
 }
