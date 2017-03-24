a4520aaad920e918ef016f0a1ab9a478d546c73b
CodingWorker <dy10010@sina.cn>
2017-03-24 23:47:08 +0800

    java

---
+++ JAVA/codes/xiaomage_codes/Day08/src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * Created by DaiYan on 2017/3/24.
+ */
+public class Main {
+    public static void main(String[] args){
+        Cat cat=new Cat();
+        cat.name=12;
+        System.out.println(cat.name);
+    }
+}
---
+++ JAVA/xiaomage_jiaoyu/codes/Chapter08/src/Dog.java
@@ -0,0 +1,9 @@
+/**
+ * Created by DaiYan on 2017/3/24.
+ */
+public class Dog extends Animal{
+    @Override
+    public void eat(){
+        System.out.println("dog is eating ...");
+    }
+}
