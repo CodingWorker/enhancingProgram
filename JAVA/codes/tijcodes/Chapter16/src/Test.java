fbac11f103b62d54cb7d12aa9e1587c11a271a3a
CodingWorker <dy10010@sina.cn>
2017-03-31 08:35:15 +0800

    java

---
+++ JAVA/codes/tijcodes/Chapter16/src/Test.java
@@ -0,0 +1,13 @@
+import java.util.Vector;
+
+/**
+ * Created by DaiYan on 2017/3/31.
+ */
+public class Test {
+    public static void main(String[] args){
+        Vector
+                a=new Vector(),
+                b=new Vector();
+
+    }
+}
