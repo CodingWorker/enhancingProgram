56b7295eb300ec01c3612d0bb6bb6de2501cf4a5
CodingWorker <dy10010@sina.cn>
2017-04-26 00:05:24 +0800

    add

---
+++ JAVA/efficientJava/office_codes/mycodes/src/chapter04/item19/ToolsContstant.java
@@ -0,0 +1,15 @@
+package chapter04.item19;
+
+/**
+ * Created by DaiYan on 2017/4/25.
+ */
+
+/**
+ * 使用不可以实例化的常量来导出常量
+ */
+public class ToolsContstant {
+    private ToolsContstant(){}
+
+    public static final String A="";
+    public static final String B="";
+}
