c1358c7d5c1dedde588e33fc8bfb55ee99d35a02
DaiYan <DaiYan@heng.zhun.com.cn>
2017-06-15 12:03:16 +0800

    add

---
+++ JAVA/tij/CodesDemo/src/chapter10/Test1.java
@@ -0,0 +1,12 @@
+package chapter10;
+
+import java.io.InputStream;
+
+/**
+ * Created by DaiYan on 2017/6/15.
+ */
+public class Test1 {
+    public static void miain(String[] args){
+        new InputStream()
+    }
+}
